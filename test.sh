8dcbb800fe703ab027b11e36c8ea06a543f3b920
STARC00KIE <os4155@gmail.com>
2025-10-27 21:18:56 -0400

    feat: chandraocr 단독 실행

--- test.sh
+++ test.sh
@@ -3,28 +3,28 @@
 source /home/vtw/workspace/yes/etc/profile.d/conda.sh
 
 # olmocr-test 경로 이동
-cd olm
+# cd olm
 
 # olmocr-test 테스트 수행
-conda activate olmocr-test
-python --version
-echo "olmocr 테스트 시작"
-python olmocr_test.py
-echo "olmocr 테스트 종료"
+# conda activate olmocr-test
+# python --version
+# echo "olmocr 테스트 시작"
+# python olmocr_test.py
+# echo "olmocr 테스트 종료"
 
 # deepseekocr 경로 이동
-cd ..
-cd deepseekocr
+# cd ..
+# cd deepseekocr
 
 # 딥시크 ocr 테스트 수행
-conda activate deepseek-ocr
-python --version
-echo "deepseek-ocr 테스트 시작"
-python deepseekocr_test.py
-echo "deepseek-ocr 테스트 종료"
+# conda activate deepseek-ocr
+# python --version
+# echo "deepseek-ocr 테스트 시작"
+# python deepseekocr_test.py
+# echo "deepseek-ocr 테스트 종료"
 
 # chandraocr 경로 이동
-cd ..
+# cd ..
 cd chandraocr
 
 # 찬드라 ocr 테스트 수행
